9b9b721e1f7abd82b964391d6b3795bab8160580
asad <asadhamasy@gmail.com>
2023-12-23 22:10:24 +0700

    style: fixing card in dark mode

--- app/src/main/java/com/asad/newsapi/data/network/response/Source.kt
+++ app/src/main/java/com/asad/newsapi/data/network/response/Source.kt
@@ -2,9 +2,9 @@ package com.asad.newsapi.data.network.response
 
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
-import kotlinx.android.parcel.Parcelize
 
-@Parcelize
+
+@kotlinx.parcelize.Parcelize
 data class Source(
 
 	@field:SerializedName("name")
